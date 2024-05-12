ef66333221ddec1c1316e5e266301a9a0f020dfd
Para Thunder <143155355+Para-thunder@users.noreply.github.com>
2024-05-12 14:52:16 +0500

    Update

--- Activity.h
+++ Activity.h
@@ -2,53 +2,21 @@
 #ifndef ACTIVITY_H
 #define ACTIVITY_H
 
-#include <iostream>
-#include <fstream>
-#include<string>
-
+#include"Manager.h"
+#include<fstream>
+#include<iostream>
 using namespace std;
-class Activity {
+
+class Activity
+{
 	int type;
-	string value;
-
+	char* value;
 public:
-	Activity()
-	{
-		type = 0;
-		value = "";
-	}
-	~Activity(){
-		
-	}
-	void Print()
-	{
-		if (type == 1)
-		{
-			cout << " feeling " << endl;
-		}
-		else if (type == 2)
-		{
-			cout << " thinking about " << value;
-		}
-		else if (type == 3)
-		{
-			cout << "Making" << value;
-		}
+	Activity();
+	~Activity();
+	void Print();
+	void ReadDataFromFile(ifstream& inp);
+	void SetValue(char* text);
 
-		else if (type == 4)
-		{
-			cout << " celebrating " << value;
-		}
-	}
-	void ReadDataFromFile(ifstream& inp)
-	{
-		getline(inp, value);
-	}
-	void SetValue(string& text)
-	{
-		value = text;
-	}
 };
-
-
 #endif
